dbbce5f7e8e908f64ccc7ecaae9605958fb768c3
Kunal Tyagi <tyagi.kunal@live.com>
2017-09-13 16:01:07 +0900

    Added padding and error message in gen_salt

--- ssec.erl
+++ ssec.erl
@@ -8,9 +8,13 @@
 
 -import(crypto, [strong_rand_bytes/1]).
 
-gen_salt(Length) ->
+gen_salt(Length) when Length >= 32, Length =< 64 ->
     % erlang:binary_to_list(crypto:strong_rand_bytes(Length)).
-    crypto:strong_rand_bytes(Length).
+    crypto:strong_rand_bytes(Length);
+gen_salt(Length) when Length < 32 ->
+    {error, "Minimum length is 32. Provided: " ++ erlang:integer_to_list(Length)};
+gen_salt(Length) ->
+    {error, "Maximum length is 64. Provided: " ++ erlang:integer_to_list(Length)}.
 
 gen_hash(Type, UserKey, Salt) ->
     % save the algorithm used, eg md5, sha, sha256
@@ -31,8 +35,26 @@ algo_metadata() ->
 
 encrypt_data(UserKey, Data) ->
     Algo = element(1, algo_metadata()),
-    crypto:block_encrypt(Algo, UserKey, Data).
+    PadData = pad_rfc5652(16, Data),
+    crypto:block_encrypt(Algo, UserKey, PadData).
 
 decrypt_data(UserKey, Data) ->
     Algo = element(1, algo_metadata()),
     crypto:block_decrypt(Algo, UserKey, Data).
+
+pad_zero(Width, Binary) ->
+    case (Width - (size(Binary) rem Width)) rem Width of
+        0 -> Binary;
+        N -> <<Binary/binary, 0:(N*8)>> % 8 bits in one byte
+    end.
+
+pad_rfc5652(Width, Binary) ->
+    case (Width - (size(Binary) rem Width)) rem Width of
+        0 -> pad_rfc5652(Width, Width, Binary);
+        N -> pad_rfc5652(N, N, Binary)
+    end.
+
+pad_rfc5652(OrigWidth, Length, Binary) when Length > 0 ->
+    pad_rfc5652(OrigWidth, Length-1, <<Binary/binary, OrigWidth:8>>);
+pad_rfc5652(_, 0, Binary) ->
+    Binary.
