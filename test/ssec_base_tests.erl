b7f61f280d649ff219186ed482003864849a3a91
Kunal Tyagi <tyagi.kunal@live.com>
2017-09-15 19:04:50 +0900

    Moved a test case to its proper place

--- test/ssec_base_tests.erl
+++ test/ssec_base_tests.erl
@@ -4,8 +4,6 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
--import(ssec_base, [gen_salt/1]).
-
 -ifdef(EUNIT).
 
 %% Test 1
@@ -13,5 +11,15 @@ gen_salt_test_() ->
     [?_assert(gen_salt(32) == gen_salt(32)),
      ?_assert(gen_salt(32) /= gen_salt(32))].
      
+hash_test_() ->
+    [
+     fun()->
+         AlgoList = [md5, sha, sha256],
+         {ok, Key} = gen_salt(64),
+         {ok, Salt} = gen_salt(32),
+         HashList = [gen_hash(Algo, Key, Salt) || Algo <- AlgoList],
+         ?_assert(lists:all(fun(X) -> verify_key(Key, Salt, X) =:= true end, HashList))
+     end
+    ].
 
 -endif.
