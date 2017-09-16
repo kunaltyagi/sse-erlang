21b6afe45d80c8360ee87469fa24128d8c010229
Kunal Tyagi <tyagi.kunal@live.com>
2017-09-16 21:00:49 +0900

    Adding specs and documentation, WIP

--- src/ssec_base.erl
+++ src/ssec_base.erl
@@ -2,15 +2,18 @@
 
 -author("kunal.tyagi").
 
+-ifdef(TEST).
 -include("eunit/include/eunit.hrl").
+-endif().
 
 -export([gen_salt/1,
          gen_hash/3,
          verify_key/3,
-         encrypt_data/2, encrypt_data/3,
-         decrypt_data/2, decrypt_data/3,
-         test_hash/0,
-         test_encryption/0]).
+         block_encrypt_data/2, block_encrypt_data/3,
+         block_decrypt_data/2, block_decrypt_data/3,
+         stream_encrypt_data/2, stream_encrypt_data/3,
+         stream_decrypt_data/2, stream_decrypt_data/3
+        ]).
 
 %% @doc generate a random salt
 %%
@@ -29,11 +32,11 @@ gen_salt(Length) ->
 %% @doc generate hash of user key using the salt
 %%
 -spec(gen_hash(Type, UserKey, Salt) ->
-        {Type, Mac} when Type::hash_algorithms() - except ripemd160,
+        {Type, Mac} when Type::hash_algorithms(),
+                         %% except ripemd160,
                          UserKey::iodata(),
                          Salt::iodata(),
                          Mac::binary()).
-hash_algorithms() - except ripemd160
 gen_hash(Type, UserKey, Salt) ->
     % save the algorithm used, eg md5, sha, sha256
     {Type, crypto:hmac(Type, Salt, UserKey)}.
@@ -44,19 +47,21 @@ gen_hash(Type, UserKey, Salt) ->
         true | false when UserKey::iodata(),
                           Salt::iodata(),
                           Hash::{Type, binary()},
-                          Type::hash_algorithms() - except ripemd160).
+                          Type::hash_algorithms()).
+                          %% except ripemd160
 verify_key(UserKey, Salt, Hash) ->
     {HashType, _HashValue} = Hash,
     gen_hash(HashType, UserKey, Salt) =:= Hash.
 
 %% @doc sample implementation for providing custom algorithms to
-%%      encrypt and decrypt in a uniform manner
+%%      encrypt and decrypt in a uniform manner. Use proper algorithm type for
+%%      block and stream functions
 %%
 -spec(algo_metadata() ->
-        {AlgoType, PadAlgo} where AlgoType = des_ecb | blowfish_ecb | aes_ecb,
-                                  PadAlgo = zero | rfc5652).
+        {AlgoType, PadAlgo} when AlgoType::rc4|des_ecb|blowfish_ecb|aes_ecb,
+                                  PadAlgo::zero|rfc5652).
 algo_metadata() ->
-    % store details such as 
+    % store details such as
     % - algorithm to use
     % - use IVec or not
     % - use AEAD mode or not
@@ -70,10 +75,10 @@ algo_metadata() ->
 %% @doc block encrypt data using the user key
 %%
 -spec(block_encrypt_data(UserKey, Data, AlgoMetaData) ->
-    CipherPadData where UserKey = block_key(),
-                        Data = io_data(),
-                        AlgoMetaData = algo_metadata(),
-                        CipherPadData = binary()).
+        CipherPadData when UserKey::block_key(),
+                            Data::io_data(),
+                            AlgoMetaData::algo_metadata(),
+                            CipherPadData::binary()).
 block_encrypt_data(UserKey, Data) ->
     block_encrypt_data(UserKey, Data, algo_metadata()).
 block_encrypt_data(UserKey, Data, AlgoMetaData) ->
@@ -82,13 +87,45 @@ block_encrypt_data(UserKey, Data, AlgoMetaData) ->
     PadData = pad(Pad, 16, Data),
     crypto:block_encrypt(Algo, UserKey, PadData).
 
+%% @doc stream encrypt data using the user key
+%%
+-spec(stream_encrypt_data(UserKey, Data, AlgoMetaData) ->
+        {State, CipherData} when UserKey::{key, io_data()} | State,
+                                  Data::io_data(),
+                                  AlgoMetaData::algo_metadata(),
+                                  State::{state, opaque()},
+                                  CipherData::binary()).
+stream_encrypt_data({state, OldState}, Data) ->
+    {NewStream, CipherData} = crypto:stream_encrypt(OldState, Data),
+    {{state, NewStream}, CipherData};
+stream_encrypt_data({key, UserKey}, Data) ->
+    stream_encrypt_data({key, UserKey}, Data, algo_metadata()).
+stream_encrypt_data({key, UserKey}, Data, AlgoMetaData) ->
+    {Algo, _} = AlgoMetaData,
+    stream_encrypt_data({state, crypto:stream_init(Algo, UserKey)}, Data).
+
+%% @doc stream encrypt data using the user key and init vector
+%%
+-spec(stream_encrypt_data(UserKey, Data, IVec, AlgoMetaData) ->
+        {State, CipherData} when UserKey::{key, io_data()},
+                                  Data::io_data(),
+                                  IVec::binary(),
+                                  AlgoMetaData::algo_metadata(),
+                                  State::{state, opaque()},
+                                  CipherData::binary()).
+%stream_encrypt_data({key, UserKey}, Data, IVec) ->
+%    stream_encrypt_data(UserKey, Data, IVec, algo_metadata()).
+stream_encrypt_data({key, UserKey}, Data, IVec, AlgoMetaData) ->
+    {Algo, _} = AlgoMetaData,
+    stream_encrypt_data({state, crypto:stream_init(Algo, UserKey, IVec)}, Data).
+
 %% @doc block decrypt data using the user key
 %%
 -spec(block_decrypt_data(UserKey, Data, AlgoMetaData) ->
-        PlainData where UserKey = block_key(),
-                        Data = binary(),
-                        AlgoMetaData = algo_metadata(),
-                        PlainData = iodata()).
+        PlainData when UserKey::block_key(),
+                        Data::binary(),
+                        AlgoMetaData::algo_metadata(),
+                        PlainData::iodata()).
 block_decrypt_data(UserKey, Data) ->
     block_decrypt_data(UserKey, Data, algo_metadata()).
 block_decrypt_data(UserKey, Data, AlgoMetaData) ->
@@ -96,26 +133,50 @@ block_decrypt_data(UserKey, Data, AlgoMetaData) ->
     PadData = crypto:block_decrypt(Algo, UserKey, Data),
     unpad(Pad, PadData).
 
+%% @doc stream decrypt data using the user key
+%%
+-spec(stream_decrypt_data(UserKey, Data, AlgoMetaData) ->
+        {State, CipherData} when UserKey::{key, io_data()} | State,
+                                  Data::io_data(),
+                                  AlgoMetaData::algo_metadata(),
+                                  State::{state, opaque()},
+                                  CipherData::binary()).
+stream_decrypt_data({state, OldState}, Data) ->
+    crypto:stream_decrypt(OldState, Data);
+stream_decrypt_data({key, UserKey}, Data) ->
+    stream_decrypt_data(UserKey, Data, algo_metadata()).
+stream_decrypt_data({key, UserKey}, Data, AlgoMetaData) ->
+    {Algo, _} = AlgoMetaData,
+    stream_decrypt_data({state, crypto:stream_init(Algo, UserKey)}, Data).
+
+%% @doc stream decrypt data using the user key and init vector
+%%
+-spec(stream_decrypt_data(UserKey, Data, IVec, AlgoMetaData) ->
+        {State, CipherData} when UserKey::{key, io_data()} | State,
+                                  Data::io_data(),
+                                  IVec::binary(),
+                                  AlgoMetaData::algo_metadata(),
+                                  State::{state, opaque()},
+                                  CipherData::binary()).
+%stream_decrypt_data({key, UserKey}, Data, IVec) ->
+%    stream_decrypt_data(UserKey, Data, IVec, algo_metadata()).
+stream_decrypt_data({key, UserKey}, Data, IVec, AlgoMetaData) ->
+    {Algo, _} = AlgoMetaData,
+    stream_decrypt_data({state, crypto:stream_init(Algo, UserKey, IVec)}, Data).
+
 %% @doc verify that the data is encryted correctly
 %%
 -spec(verify_block_encryption(Key, Msg, AlgoMetaData) ->
-        {true|false, EncryptedMsg} when Key = block_key(),
-                                        Msg = io_data(),
-                                        AlgoMetaData = algo_metadata(),
-                                        EncryptedMsg = binary()).
+        {Status, EncryptedMsg} when Key::block_key(),
+                                    Msg::io_data(),
+                                    AlgoMetaData::algo_metadata(),
+                                    Status::true|false,
+                                    EncryptedMsg::binary()).
 verify_block_encryption(Key, Msg, AlgoMetaData) ->
     EncryptedMsg = block_encrypt_data(Key, Msg, AlgoMetaData),
     {Msg =:= block_decrypt_data(Key, EncryptedMsg, AlgoMetaData),
          EncryptedMsg}.
-
-test_encryption() ->
-    AlgoList = [aes_ecb],
-    PadType = [zero, rfc5652],
-    {ok, Key} = gen_salt(32),
-    Msg = <<"Test Binary Stream">>,
-    MetaDataList = [{Algo, Pad} || Algo <- AlgoList, Pad <- PadType],
-    lists:all(fun(X) -> verify_encryption(Key, Msg, X) =:= {true, _} end,
-              MetaDataList).
+% @TODO verify_stream_encryption
 
 pad(zero, Width, Binary) ->
     pad_zero(Width, Binary);
@@ -149,17 +210,36 @@ unpad_zero(Binary, Idx) ->
     _ -> binary:part(Binary, 0, Idx + 1)
   end.
 
-pad_rfc5652(Width, Binary) ->
+%% @doc pad data padded a per RFC5652
+%%      @see pad_rfc5652/3
+%% @private
+-spec(pad_rfc5652(Width, Binary) ->
+        PaddedBinary when Width::integer(),
+                          Binary::binary(),
+                          PaddedBinary::binary()).
+pad_rfc5652(Width, Binary) when Width /= 0 ->
     case (Width - (size(Binary) rem Width)) rem Width of
         0 -> pad_rfc5652(Width, Width, Binary);
         N -> pad_rfc5652(N, N, Binary)
     end.
 
+%% @doc pads by appending one byte at a time till Length bytes are added
+%% @private
+-spec(pad_rfc5652(OrigWidth, Length, Binary) ->
+        PaddedBinary when OrigWidth::integer(),
+                          Length::integer(),
+                          Binary::binary(),
+                          PaddedBinary::binary()).
 pad_rfc5652(_OrigWidth, 0, Binary) ->
     Binary;
 pad_rfc5652(OrigWidth, Length, Binary) ->
-    pad_rfc5652(OrigWidth, Length-1, <<Binary/binary, OrigWidth:8>>).
+    pad_rfc5652(OrigWidth, Length - 1, <<Binary/binary, OrigWidth:8>>).
 
+%% @doc unpad data padded a per RFC5652
+%% @private
+-spec(unpad_rfc5652(Binary) ->
+        UnpaddedBinary when Binary::binary(),
+                            UnpaddedBinary::binary()).
 unpad_rfc5652(Binary) ->
     Size = size(Binary),
     Last = binary:at(Binary, Size - 1),
