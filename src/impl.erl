5073203a7f0069986f0dcb18b09a0f03587d375f
Kunal Tyagi <tyagi.kunal@live.com>
2017-09-18 02:18:30 +0900

    Adding API func. WIP

--- src/impl.erl
+++ src/impl.erl
@@ -1,5 +1,7 @@
 -module(impl).
 
+-author("kunal.tyagi").
+
 -import(ssec_base, [gen_salt/1, gen_hash/3, verify_key/3, encrypt_data/2,
                     encrypt_data/3, decrypt_data/2, decrypt_data/3]).
 
@@ -8,7 +10,8 @@
          test_verification_ssec_key/0]).
 
 verify_ssec_algorithm(Algorithm) ->
-    Algorithm =:= "AES256".
+    ValidAlgorithms = ["AES256"],
+    {lists:member(Algorithm, ValidAlgorithms), ValidAlgorithms}.
 
 verify_ssec_key(ASCIIKey, Checksum) ->
     % is_list(ASCIIKey)?
@@ -25,9 +28,6 @@ verify_ssec_key(ASCIIKey, Checksum) ->
         size(HashValue) /= 128/4 ->
            {false, "MD5 checksum is not 128 bit long. Provided: " ++ erlang:integer_to_list(size(Key))};
         true ->
-% TODO:
-% crypto gives <<8 bit integers comma seperated>>
-% HashValue is <<"Base16string">>
             Lhs = erlang:binary_to_list(crypto:hash(HashType, Key)),
             Rhs = erlang:binary_to_integer(HashValue, 16),
             Value = lists:foldl(fun(X, Old) -> X + Old*256 end, 0, Lhs) =:= Rhs,
@@ -38,3 +38,23 @@ test_verification_ssec_key() ->
     Key = "556B58703273357638792F413F4428472B4B6250655368566D59713374367739",
     Checksum = "64C40DC99A6FE92CF3B7CBD5C22D8A13",
     verify_ssec_key(base64:encode(Key), {md5, base64:encode(Checksum)}).
+
+get_operation(ObjectDetails, RequestHeaders) ->
+    {Algorithm, Key, Checksum} = RequestHeaders,
+    Md5Checksum = {md5, Checksum},
+    get_operation({algo, verify_ssec_algorithm(Algorithm)});
+get_operation({algo, {AlgoStatus, _AlgoList}}) ->
+    if
+        AlgoStatus =:= false ->
+            {false, "Expected " ++ AlgoList ++ "Provided: " ++ Algorithm};
+        true ->
+            {KeyStatus, KeyMessage} = verify_ssec_key(Key, Md5Checksum),
+            if
+                KeyStatus =:= false ->
+                    {KeyStatus, Message};
+                true ->
+                    {Salt, Hash, Data} = getObject(ObjectDetails),
+                    ssec_base:verify_key
+
+getObject(ObjectDetails) ->
+    {false, "TODO. Not implemented"}.
